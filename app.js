aaea73baf758afa38735c206780decc9b127dc7d
Shaher-11 <shahershamroukh@gmail.com>
2021-07-05 16:35:18 +0200

    Add search el

---
+++ app.js
@@ -0,0 +1,9 @@
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (e) => {
+  const userText = e.target.value;
+
+  if(userText !== ''){
+    console.log(userText);
+  }
+});
